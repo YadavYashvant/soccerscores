cc5f9f7b6efa627aef5e5a0dbc1e06e90b111462
Yashvant Yadav <113130559+YadavYashvant@users.noreply.github.com>
2023-06-27 19:27:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function fun1() {
+    document.getElementById("demo").innerHTML = "Content of this block changed, now click show time button to show time again :) ";
+}
+function fun2() {
+    alert("This website is a mess isn't it :)");
+}
